edd603693d504f455e84ac89ac4803de2950bb1c
Rishabh <jharishabh2k3@gmail.com>
2023-03-30 21:01:40 +0530

    fix(first-and-last-occurance.cpp): formatted the code

--- Example-Code/first-and-last-ocurrence.cpp
+++ Example-Code/first-and-last-ocurrence.cpp
@@ -1,60 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int firstOcc(int *arr, int size, int target)
-{
+int firstOcc(int *arr, int size, int target) {
     int start = 0;
     int end = size - 1;
     int mid;
     int ans = -1;
-    while (start <= end)
-    {
+    while (start <= end) {
         mid = start + (end - start) / 2;
-        if (arr[mid] == target)
-        {
+        if (arr[mid] == target) {
             ans = mid;
             end = mid - 1;
-        }
-        else if (arr[mid] > target)
-        {
+        } else if (arr[mid] > target) {
             end = mid - 1;
-        }
-        else
-        {
+        } else {
             start = mid + 1;
         }
     }
     return ans;
 }
 
-int lastOcc(int *arr, int size, int target)
-{
+int lastOcc(int *arr, int size, int target) {
     int start = 0;
     int end = size - 1;
     int mid;
-    int ans = -1; 
-    while (start <= end)
-    {
+    int ans = -1;
+    while (start <= end) {
         mid = start + (end - start) / 2;
-        if (arr[mid] == target)
-        {
+        if (arr[mid] == target) {
             ans = mid;
             start = mid + 1;
-        }
-        else if (arr[mid] > target)
-        {
+        } else if (arr[mid] > target) {
             end = mid - 1;
-        }
-        else
-        {
+        } else {
             start = mid + 1;
         }
     }
     return ans;
 }
 
-int main()
-{
+int main() {
     int arr[8] = {1, 2, 3, 3, 3, 3, 4, 5};
     cout << "First Occurrence at: " << firstOcc(arr, 8, 3) << endl;
     cout << "Last Occurrence at: " << lastOcc(arr, 8, 3);
