ce14ae1ad911d82b2555d042b597e8527335df55
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-14 10:31:03 +0530

    formatted some cpp code

--- Data-Structures/Trees/problems/check-for-balanced-bt.cpp
+++ Data-Structures/Trees/problems/check-for-balanced-bt.cpp
@@ -2,56 +2,57 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
 };
 
 class Solution {
-public:
-  // a function to check if tree is balanced or not on basis of it's height.
-  bool isBalanced(TreeNode *root) { return dfsHeight(root) != -1; }
+  public:
+    // a function to check if tree is balanced or not on basis of it's height.
+    bool isBalanced(TreeNode* root) { return dfsHeight(root) != -1; }
 
-  /* a function to calculate height of tree, if this function returns -1,
-    the tree is not balanced. The tree will always be balanced if this function
-    returns some non-negative height */
+    /* a function to calculate height of tree, if this function returns -1,
+      the tree is not balanced. The tree will always be balanced if this
+      function returns some non-negative height */
 
-  int dfsHeight(TreeNode *root) {
-    if (root == NULL)
-      return 0;
+    int dfsHeight(TreeNode* root) {
+        if (root == NULL)
+            return 0;
 
-    int lh = dfsHeight(root->left);
-    if (lh == -1)
-      return -1;
+        int lh = dfsHeight(root->left);
+        if (lh == -1)
+            return -1;
 
-    int rh = dfsHeight(root->right);
-    if (rh == -1)
-      return -1;
+        int rh = dfsHeight(root->right);
+        if (rh == -1)
+            return -1;
 
-    // for a balanced BT, height(left sub-tree) - height(right sub-tree) <= 1
-    if (abs(lh - rh) > 1)
-      return -1;
+        // for a balanced BT, height(left sub-tree) - height(right sub-tree) <=
+        // 1
+        if (abs(lh - rh) > 1)
+            return -1;
 
-    return max(lh, rh) + 1;
-  }
+        return max(lh, rh) + 1;
+    }
 };
 
 int main() {
-  Solution solution;
+    Solution solution;
 
-  TreeNode *balancedTree = new TreeNode(1);
-  balancedTree->left = new TreeNode(2);
-  balancedTree->right = new TreeNode(3);
-  balancedTree->left->left = new TreeNode(4);
-  balancedTree->left->right = new TreeNode(5);
-  balancedTree->right->left = new TreeNode(6);
-  balancedTree->right->right = new TreeNode(7);
+    TreeNode* balancedTree = new TreeNode(1);
+    balancedTree->left = new TreeNode(2);
+    balancedTree->right = new TreeNode(3);
+    balancedTree->left->left = new TreeNode(4);
+    balancedTree->left->right = new TreeNode(5);
+    balancedTree->right->left = new TreeNode(6);
+    balancedTree->right->right = new TreeNode(7);
 
-  bool isBalancedResult = solution.isBalanced(balancedTree);
-  cout << "tree balanced? " << (isBalancedResult ? "Yes" : "No") << endl;
-  return 0;
+    bool isBalancedResult = solution.isBalanced(balancedTree);
+    cout << "tree balanced? " << (isBalancedResult ? "Yes" : "No") << endl;
+    return 0;
 }
--- Data-Structures/Trees/problems/symmetric-bt.cpp
+++ Data-Structures/Trees/problems/symmetric-bt.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
 /*
@@ -14,36 +14,37 @@ the root node are mirror images of each other.
 */
 
 class Solution {
-public:
-  bool isSymmetric(TreeNode *root) {
-    return root == NULL || helper(root->left, root->right);
-  }
-  bool helper(TreeNode *left, TreeNode *right) {
-    if (left == NULL || right == NULL)
-      return left == right;
-    if (left->val != right->val)
-      return false;
-    return helper(left->left, right->right) && helper(left->right, right->left);
-  }
+  public:
+    bool isSymmetric(TreeNode* root) {
+        return root == NULL || helper(root->left, root->right);
+    }
+    bool helper(TreeNode* left, TreeNode* right) {
+        if (left == NULL || right == NULL)
+            return left == right;
+        if (left->val != right->val)
+            return false;
+        return helper(left->left, right->right) &&
+               helper(left->right, right->left);
+    }
 };
 
 int main() {
-  TreeNode *root = new TreeNode(1);
-  root->left = new TreeNode(2);
-  root->right = new TreeNode(2);
-  root->left->left = new TreeNode(3);
-  root->left->right = new TreeNode(4);
-  root->right->left = new TreeNode(4);
-  root->right->right = new TreeNode(3);
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
 
-  Solution solution;
-  bool isSymmetric = solution.isSymmetric(root);
+    Solution solution;
+    bool isSymmetric = solution.isSymmetric(root);
 
-  if (isSymmetric) {
-    cout << "The tree is symmetric." << endl;
-  } else {
-    cout << "The tree is not symmetric." << endl;
-  }
+    if (isSymmetric) {
+        cout << "The tree is symmetric." << endl;
+    } else {
+        cout << "The tree is not symmetric." << endl;
+    }
 
-  return 0;
+    return 0;
 }
