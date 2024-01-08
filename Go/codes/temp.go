69872b07f63733f737b2c2dc0e5d277840a9adc9
amit <sutharamit707@gmail.com>
2024-01-09 00:22:13 +0530

    arrays-slices

--- Go/codes/temp.go
+++ Go/codes/temp.go
@@ -2,47 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type Shape interface {
-	area() float64
-}
-type Circle struct {
-	radius float64
-}
-
-type Squ struct {
-	side float64
-}
-
-func (c Circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (s Squ) area() float64 {
-	return s.side * s.side
-}
-
-func printDetails(s Shape) {
-	switch v := s.(type) {
-	case Circle:
-		fmt.Printf("Area: %f \n", v.area())
-	default:
-		fmt.Println("Imposter!")
-	}
-}
-
 func main() {
-	var radius float64
-	fmt.Print("Enter Radius: ")
-	fmt.Scanf("%f", &radius)
-	circle := Circle{
-		radius: radius,
-	}
-	squ := Squ{
-		side: 10,
-	}
-	printDetails(circle)
-	printDetails(squ)
+	array := [5]int{1, 2, 3, 4, 5}
+	slice := array[1:4]
+	fmt.Println(array)
+	fmt.Println(slice)
+
+	fmt.Printf("Length: %v", len(slice))
+	fmt.Printf("\nCapacity: %v", cap(slice))
 }
