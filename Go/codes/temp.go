2b49bfbc50bd256ad2c2c78c7240812fddc94df6
amit <sutharamit707@gmail.com>
2024-01-13 22:47:55 +0530

    practice goroutines

--- Go/codes/temp.go
+++ Go/codes/temp.go
@@ -5,25 +5,41 @@ import (
 	"time"
 )
 
-func getResult(resultChan chan<- int) {
-	time.Sleep(time.Second) // Simulating some work
-	resultChan <- 42
+func getUserId(id chan<- int) {
+	time.Sleep(time.Second)
+	id <- 69
+}
+
+func getUserById(id int) string {
+	if id == 69 {
+		return "Amit"
+	}
+	return "user not found"
 }
 
 func main() {
-	fmt.Println("goroutines")
-	resultChannel := make(chan int)
-	go getResult(resultChannel)
-	result := <-resultChannel
-	fmt.Println(result)
+	idChannel := make(chan int)
+	go getUserId(idChannel)
 
-	c1 := make(chan int)
+	fmt.Println("fetching user id...")
+	userId := <-idChannel
 
-	fmt.Println("push 69 into channel")
-	go func() {
-		c1 <- 69
-	}()
+	fmt.Println("fetching User by id:", userId)
+	user := getUserById(userId)
 
-	g1 := <-c1
-	fmt.Println("Got from channel: ", g1)
+	fmt.Println("User: ", user)
 }
+
+/*
+- flow of execution :
+1. main function runs,
+2. idChannel gets spawned,
+3. a new goroutine named getUserId is launched concurrently with main goroutine,
+4. getUserId goroutine gets blocked for a second, but the main goroutine still runs,
+5. after a second, getUserId gouroutne sends 69 into the idChannel,
+6. Meanwhile, the main goroutine prints the message "fetching user id..." to the console,
+7. The main goroutine then receives the value from the idChannel channel using userId := <-idChannel.
+8. The getUserById function checks if the user ID is 69 and returns,
+9. The main goroutine prints the retrieved user information to the console,
+10. The execution completes.
+*/
